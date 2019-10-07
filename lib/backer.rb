3a74f9a98e78c1825f7c62919aa08b9840bf240a
Srikalyan Megati <kalmegati@gmail.com>
2019-10-07 17:56:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
   
-  attr_reader :name, :backed_projects
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   
-  def back_project
-    
+  def back_project(project)
+    project.
   end
   
 end
